3d0e9ef1665557fbb37342922c843367b79bc4c7
djiro-256 <info@d-nakazawa.com>
2019-01-27 21:26:11 +0900

    first commit

--- myapp/views.py
+++ myapp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world") #hello world
+    return HttpResponse("Hello, world") #Hello world
